b131beb243396804163c8c427ae4abc7b4c748f8
Kathy Bauer <kbauer17@my.wctc.edu>
2023-04-03 13:35:33 -0500

    [1.08] Use appsettings.json **** MAKE TO MODIFY .gitignore **** dotnet add package Microsoft.Extensions.Configuration -v 6.0.1 dotnet add package Microsoft.Extensions.Configuration.Json -v 6.0.0 - Create appsettings.json - Ensure appsettings.json is copied to output directory - Update DbContext, use appsettings.json for connection string

--- DbContext.cs
+++ DbContext.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 public class BloggingContext : DbContext
 {
@@ -13,13 +15,10 @@ public class BloggingContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        // *******************************************************************
-        // * IT IS NOT A GOOD IDEA TO STORE USER ID AND/OR PASSWORD          *
-        // * DIRECTLY IN A CLASS (ESPECIALLY IF IT WILL BE POSTED ON GITHUB) *
-        // * WE WILL LEARN LATER HOW TO REMOVE THIS DATA FROM THE CLASS      *
-        // *******************************************************************
-        // ## last 2 digits of CRN
-        // XXX your initials
-        optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=BlogsConsole_##_XXX;User ID=YYY;Password=ZZZ");
+        var configuration =  new ConfigurationBuilder()
+        .AddJsonFile($"appsettings.json");
+
+        var config = configuration.Build();
+        optionsBuilder.UseSqlServer(@config["BlogsConsole:ConnectionString"]);
     }
 }
