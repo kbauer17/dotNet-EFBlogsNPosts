e27bf6246938c620d21477b3cdadc8f9c9abdbeb
Kathy Bauer <kbauer17@my.wctc.edu>
2023-04-04 13:01:05 -0500

    ]2.06] Added outermost looping

--- Program.cs
+++ Program.cs
@@ -11,139 +11,145 @@ logger.Info("Program started");
 string choice = "";
 try
 {
-    Console.ForegroundColor = ConsoleColor.Green;
+    do{
+        Console.ForegroundColor = ConsoleColor.Green;
 
-    // Ask which type of item to display
-    Console.WriteLine("\nSelect an activity:\n1) Display all blogs\n2) Add Blog\n3) Create Post\n4) Display Posts");
+        // Ask which type of item to display
+        Console.WriteLine("\nSelect an activity:\n1) Display all blogs\n2) Add Blog\n3) Create Post\n4) Display Posts\n5) Exit");
 
-    // input selection
-    choice = Console.ReadLine();
-        logger.Info("User choice: {choice}\n", choice);
-    
-    switch(choice){
-        case "1":       //Display all blogs
-            // Display all Blogs from the database
-            var db = new BloggingContext();
-            var query = db.Blogs.OrderBy(b => b.Name);
+        // input selection
+        choice = Console.ReadLine();
+            logger.Info("User choice: {choice}\n", choice);
+        
+        switch(choice){
+            case "1":       //Display all blogs
+                // Display all Blogs from the database
+                var db = new BloggingContext();
+                var query = db.Blogs.OrderBy(b => b.Name);
 
-            Console.WriteLine("All blogs in the database:");
-            foreach (var item in query)
-            {
-                Console.WriteLine(item.Name);
-            }
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Black;        
-            break;
-
-        case "2":       //Add blog
-            // Create and save a new Blog
-            Console.Write("Enter a name for a new Blog: ");
-            var name = Console.ReadLine();
-            // create an instance of Blog class
-            var blog = new Blog { Name = name };
-
-            db = new BloggingContext();
-            db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-            Console.WriteLine();
-
-            Console.ForegroundColor = ConsoleColor.Black; 
-            break;
-
-        case "3":       //Create post
-            // Determine which blog in which to create the post
-            db = new BloggingContext();
-            query = db.Blogs.OrderBy(b => b.BlogId);
-            Console.WriteLine("Select the blog in which you would like to post:");
-            foreach (var item in query)
-            {
-                Console.WriteLine(item.BlogId + " - " + item.Name);
-            }
-
-            //create an instance of Post
-            var newPost = new Post();
-
-            // input selection
-            newPost.BlogId = Convert.ToInt32(Console.ReadLine());
-                logger.Info("User choice: {newPost.BlogId}\n", newPost.BlogId);
-
-            // check if user selection is valid
-            try
-            {
-                if(newPost.BlogId < 1 || newPost.BlogId > query.Count()){
-                    throw new ArgumentOutOfRangeException($" Entry of {newPost.BlogId} is outside selection range");
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
                 }
-            }
-            catch(ArgumentOutOfRangeException arg)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                    logger.Error(arg.Message);
-                Console.WriteLine($"Error: {arg.Message}\nProgram will exit");
                 Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Black;        
+                break;
+
+            case "2":       //Add blog
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+                // create an instance of Blog class
+                var blog = new Blog { Name = name };
+
+                db = new BloggingContext();
+                db.AddBlog(blog);
+                    logger.Info("Blog added - {name}", name);
+                Console.WriteLine();
+
                 Console.ForegroundColor = ConsoleColor.Black; 
                 break;
-            }
 
-
-            Console.Write("Enter name of post >>  ");
-            newPost.Title = Console.ReadLine();
-            Console.Write("Enter content of post >>  ");
-            newPost.Content = Console.ReadLine();
-            
-            db.AddPost(newPost);
-                logger.Info("Post added - {title}", newPost.Title);
-            Console.WriteLine();
-
-            Console.ForegroundColor = ConsoleColor.Black; 
-            break;
-
-        case "4":       //Display post
-            // Determine which blog to list all posts
-            db = new BloggingContext();
-            query = db.Blogs.OrderBy(b => b.BlogId);
-            Console.WriteLine("Select the blog to display all posts:");
-            foreach (var item in query)
-            {
-                Console.WriteLine(item.BlogId + " - " + item.Name);
-            }
-
-            var subChoice = Convert.ToInt32(Console.ReadLine());
-                logger.Info("User choice: {subChoice}\n", subChoice);
-
-            // check if user selection is valid
-            try
-            {
-                if(subChoice < 1 || subChoice > query.Count()){
-                    throw new ArgumentOutOfRangeException($" Entry of {subChoice} is outside selection range");
+            case "3":       //Create post
+                // Determine which blog in which to create the post
+                db = new BloggingContext();
+                query = db.Blogs.OrderBy(b => b.BlogId);
+                Console.WriteLine("Select the blog in which you would like to post:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.BlogId + " - " + item.Name);
                 }
-            }
-            catch(ArgumentOutOfRangeException arg)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                    logger.Error(arg.Message);
-                Console.WriteLine($"Error: {arg.Message}\nProgram will exit");
+
+                //create an instance of Post
+                var newPost = new Post();
+
+                // input selection
+                newPost.BlogId = Convert.ToInt32(Console.ReadLine());
+                    logger.Info("User choice: {newPost.BlogId}\n", newPost.BlogId);
+
+                // check if user selection is valid
+                try
+                {
+                    if(newPost.BlogId < 1 || newPost.BlogId > query.Count()){
+                        throw new ArgumentOutOfRangeException($" Entry of {newPost.BlogId} is outside selection range");
+                    }
+                }
+                catch(ArgumentOutOfRangeException arg)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                        logger.Error(arg.Message);
+                    Console.WriteLine($"Error: {arg.Message}\nProgram will exit");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Black; 
+                    break;
+                }
+
+
+                Console.Write("Enter name of post >>  ");
+                newPost.Title = Console.ReadLine();
+                Console.Write("Enter content of post >>  ");
+                newPost.Content = Console.ReadLine();
+                
+                db.AddPost(newPost);
+                    logger.Info("Post added - {title}", newPost.Title);
                 Console.WriteLine();
+
                 Console.ForegroundColor = ConsoleColor.Black; 
                 break;
-            }
-            
-            var postsToDisplay = db.Posts.Where(p => p.BlogId == subChoice);
 
-            Console.WriteLine($"\nThere are {postsToDisplay.Count()} Posts in this blog:");
-            foreach (var item in postsToDisplay)
-            {
-                Console.WriteLine("Blog Name:  " + item.Blog.Name + "\tPost Title:  " + item.Title + "\t\tPost Content:  " + item.Content);
+            case "4":       //Display post
+                // Determine which blog to list all posts
+                db = new BloggingContext();
+                query = db.Blogs.OrderBy(b => b.BlogId);
+                Console.WriteLine("Select the blog to display all posts:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.BlogId + " - " + item.Name);
+                }
 
-            }
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Black;
-            break;
-        default:
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine("Invalid input detected.  Program will exit.\n");
-            Console.ForegroundColor = ConsoleColor.Black;
-            break;
-    }
+                var subChoice = Convert.ToInt32(Console.ReadLine());
+                    logger.Info("User choice: {subChoice}\n", subChoice);
+
+                // check if user selection is valid
+                try
+                {
+                    if(subChoice < 1 || subChoice > query.Count()){
+                        throw new ArgumentOutOfRangeException($" Entry of {subChoice} is outside selection range");
+                    }
+                }
+                catch(ArgumentOutOfRangeException arg)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                        logger.Error(arg.Message);
+                    Console.WriteLine($"Error: {arg.Message}\nProgram will exit");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Black; 
+                    break;
+                }
+                
+                var postsToDisplay = db.Posts.Where(p => p.BlogId == subChoice);
+
+                Console.WriteLine($"\nThere are {postsToDisplay.Count()} Posts in this blog:");
+                foreach (var item in postsToDisplay)
+                {
+                    Console.WriteLine("Blog Name:  " + item.Blog.Name + "\tPost Title:  " + item.Title + "\t\tPost Content:  " + item.Content);
+
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
+            case "5":       //Exit first time through
+                Console.WriteLine("Program will exit.\n");
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Invalid input detected.  Program will exit.\n");
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
+        }
+    }while(choice != "5");
 
 }
 catch (Exception ex)
